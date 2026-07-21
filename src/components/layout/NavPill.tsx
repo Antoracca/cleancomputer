55b7856d60b7bd5d1ac327ebd2035f5e17e99b72
Antoracca <anthonykouenilucien@gmail.com>
2026-07-21 12:56:07 +0100

    fix: remove unused Image import causing Vercel build failure

--- src/components/layout/NavPill.tsx
+++ src/components/layout/NavPill.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Menu, Search, ShoppingBag, User, X } from "lucide-react";
